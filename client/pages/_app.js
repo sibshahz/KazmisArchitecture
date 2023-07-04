e492c52e36df5e225e8f78f9141356cf7c53723f
Shahid Gillani <shahid5ssg@gmail.com>
2023-07-04 12:16:39 +0500

    Fixed responsive issue

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -16,7 +16,7 @@ export default function MyApp({ Component, pageProps }) {
         <Head>
           <meta name="viewport" content="viewport-fit=cover" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        	{/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+        	<meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <DataComponent />
         <Component {...pageProps} />
