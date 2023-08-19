7de7c61d7cb522d43b211a0e08ddb526f3b97042
Shahid Gillani <shahid5ssg@gmail.com>
2023-08-19 22:43:49 +0500

    Fixing url again

--- server/config/server.js
+++ server/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env('HOST_URL','https://kazmiarchitecture-strapi.onrender.com'), // Sets the public URL of the application.
+  url: env('HOST_URL','localhost'), // Sets the public URL of the application.
   app: {
     keys: env.array('APP_KEYS')
   },
