a57310c0faece649ee5d5ab11bad70f14446c544
Shahid Gillani <shahid5ssg@gmail.com>
2024-10-28 18:56:48 +0500

    pixel window bux fix

--- client/components/facebookPixel.js
+++ client/components/facebookPixel.js
@@ -1,6 +1,8 @@
 import ReactPixel from 'react-facebook-pixel';
 
 export const initFacebookPixel = () => {
+  if(typeof window !== 'undefined'){
   ReactPixel.init('3920104301552875');
   ReactPixel.pageView(); // For the initial page load
+  }
 };
